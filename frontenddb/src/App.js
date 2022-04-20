5102632d88f60f944a37f53cac328b418df6e1da
Jussi Wedman <juswedma@gmail.com>
2022-04-20 23:20:19 +0300

    Sivu aluilleen Header/Footer

---
+++ frontenddb/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './Navbar';
+import Footer from './Footer'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <NavbarComp>
+
+      </NavbarComp>
+      TÄHÄN JOTAIN PELEJÄ
+      <Footer>
+        
+      </Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontenddb/src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Container, Row, Col} from 'react-bootstrap'
+
+function Footer () {
+    return(
+        <Container>
+            <Row>
+                <Col className="Text-center py-3">
+                    Footer
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+export default Footer;
