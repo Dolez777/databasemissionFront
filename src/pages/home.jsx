25043a3dfb088a6426d66cea025d02895431d3b0
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-05-06 16:23:27 +0300

    home page

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Body from '../body'
+
+const URL = "http://localhost/databasemissionBack/"
+
+export default function Home() {
+  return (
+    
+    <Body url={URL}/>
+  )
+}
