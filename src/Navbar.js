5f9e9db89dc3a54682843e268d4b2f080bd51606
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-05-06 16:50:51 +0300

    Body ja filterin alku

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,19 +13,31 @@ export default class NavbarComp extends Component {
                   <Nav className="me-auto">
                     <Nav.Link href="#home">Etusivu</Nav.Link>
                     <Nav.Link href="#link">jotain</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown title="Kategoriat" id="basic-nav-dropdown">
                       <NavDropdown.Item href="#action/3.1">
-                        Action
+                      Kauhupelit
                       </NavDropdown.Item>
                       <NavDropdown.Item href="#action/3.2">
-                        Another action
+                      FPS
                       </NavDropdown.Item>
                       <NavDropdown.Item href="#action/3.3">
-                        Something
+                      Strategiapelit
                       </NavDropdown.Item>
-                      <NavDropdown.Divider />
+                      
                       <NavDropdown.Item href="#action/3.4">
-                        Separated link
+                      Ajopelit
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.4">
+                      Roolipelit
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.4">
+                      Tappelupelit
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.4">
+                      MMO
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.4">
+                      Seikkailupelit
                       </NavDropdown.Item>
                     </NavDropdown>
                   </Nav>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Navbar } from 'react-bootstrap'
 import Body from '../body'
 
 const URL = "http://localhost/databasemissionBack/"
 
 export default function Home() {
-  return (
-    
-    <Body url={URL}/>
+  const [filter, setFilter] = React.useState("0")
+    return (
+    <>
+    <Navbar url={URL} setFilter={setFilter} />
+    <Body url={URL} filter={filter}/></>
   )
 }
