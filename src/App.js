ff6ab93ee88f08b90e6c5bd2ff7c5364d5793ceb
Atte Taajoranta <n9taat00@students.oamk.fi>
2022-05-08 12:59:50 +0300

    jeee

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import NavbarComp from './components/navbar/Navbar';
-import Footer from './components/footer/Footer'
-import Body from './components/body/body';
-import Login from './login';
-import { Routes, Route } from "react-router-dom";
-import Home from './pages/home';
-import ProductDetail from './pages/ProductDetail';
-=======
 import NavbarComp from './Navbar';
 import Footer from './Footer'
 import Body from './body';
->>>>>>> parent of 48d78c5 (Merge branch 'main' into Transaction_1)
 
 const URL = 'http://localhost/databasemissionBack/';
 
